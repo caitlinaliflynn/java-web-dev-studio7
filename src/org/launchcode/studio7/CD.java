e23ab23cbeeb4f5d0da1223b2489f5bfbf9f4850
Ali <caitlinaliflynn@gmail.com>
2023-04-29 20:11:19 -0500

    finished studio 7

--- src/org/launchcode/studio7/CD.java
+++ src/org/launchcode/studio7/CD.java
@@ -1,10 +1,55 @@
 package org.launchcode.studio7;
 
-public class CD {
+public class CD extends BaseDisc {
 
     // TODO: Implement your custom interface.
 
     // TODO: Determine which fields, methods, and constructors can be extended from the base class and which ones
     //  need to be declared separately.
+
+    private final String genre;
+    private final String productionCompany;
+
+    public CD(String title, String author, String discType, String genre, String productionCompany) {
+        super(title, author, discType);
+        this.genre = genre;
+        this.productionCompany = productionCompany;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder fileList = new StringBuilder();
+        for (File file : getFiles()) {
+            fileList.append(file);
+        }
+        return super.toString() +
+                "\nGenre: " + genre +
+                "\nProduction Company: " + productionCompany +
+                "\nTracks:\n" +
+                fileList;
+    }
+
+    @Override
+    public void readData() {
+        System.out.println(this);
+    }
+
+    @Override
+    public void runFile(File file) {
+        System.out.println("\n" + file.getName() + getAuthor());
+    }
+
+    @Override
+    public void writeData(File file) {
+        if (!file.getFileType().equals("audio")) {
+            System.out.println("The" + file.getFileType() + "file type cant be written to a CD");
+        } else if (getSpaceUsed() + file.getSize() > CAPACITIES.get(getDiscType())) {
+            System.out.println("There is not enough space on the " +
+                    getDiscType() + " for " + file.getName());
+        } else {
+            getFiles().add(file);
+        }
+    }
+
 
 }
--- src/org/launchcode/studio7/Main.java
+++ src/org/launchcode/studio7/Main.java
@@ -6,7 +6,15 @@ public class Main {
 
         // TODO: Declare and initialize a CD and a DVD object.
 
+        CD threeCheers = new CD("Three Chairs for Sweet Revenge", "My Chemical Romance", "CD", "Punk", "Prod Co");
+        File helena = new File("Helena", 50, "audio");
+        File cemeteryDrive = new File("Cemetery Drive", 60, "audio");
+
         // TODO: Call each CD and DVD method to verify that they work as expected.
 
+        threeCheers.spinDisc();
+        threeCheers.readData();
+        threeCheers.writeData(helena);
+        threeCheers.writeData(cemeteryDrive);
     }
 }
